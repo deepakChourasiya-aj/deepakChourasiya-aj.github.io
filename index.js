e31c6b96332d71a72c35f5e3813a840a58980660
deepak chourasiya <deepak1812002@gmail.com>
2023-02-06 21:32:53 +0530

    added portfolio

---
+++ index.js
@@ -0,0 +1,25 @@
+let arr=[{
+    _id: 1,
+    address: "123 Main St",
+    zipcode: "12345"
+ },
+ 
+ {
+    _id: 2,
+    address: "456 Main St",
+    zipcode: "54321"
+ }
+ ,
+ {
+    _id: 3,
+    address: "789 Main St",
+    zipcode: "98765"
+ }
+ ,
+ {
+    _id: 4,
+    address: "246 Main St",
+    zipcode: "24680"
+ }]
+ arr=JSON.stringify(arr);
+ console.log(arr)
