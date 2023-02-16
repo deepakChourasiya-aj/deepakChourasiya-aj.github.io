7e354e153a149cb24d59f2101c9079b563ee05e4
deepak chourasiya <deepak1812002@gmail.com>
2023-02-16 21:32:35 +0530

    added

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-{/* <script
-  src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js">
-</script>
+var sidemenu = document.getElementById("sidemenu");
 
-<script>
-    GitHubCalendar(".calendar", "your-username");
-
-    // or enable responsive functionality:
-    GitHubCalendar(".calendar", "your-username", { responsive: true });
-
-    // Use a proxy
-    GitHubCalendar(".calendar", "your-username", {
-       proxy (username) {
-         return fetch(`https://your-proxy.com/github?user=${username}`)
-       }
-    }).then(r => r.text())
-</script> */}
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
