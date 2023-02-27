1795b15287c74c3f0c5fc37bbac03420e9d1f716
deepak chourasiya <deepak1812002@gmail.com>
2023-02-27 18:02:34 +0530

    added updated one

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function openmenu(){
 function closemenu(){
   sidemenu.style.right = "-200px";
 }
+
+function open_in_newtab(){
+  window.open("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing");
+
+}
