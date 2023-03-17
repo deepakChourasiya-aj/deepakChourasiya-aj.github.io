a45f842bbc44bccfe544c70479c22c2a8e6a8bc0
deepak chourasiya <deepak1812002@gmail.com>
2023-03-17 18:40:25 +0530

    added resume

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ function closemenu(){
 
 function open_in_newtab(){
   window.open("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing");
-
 }
+
+let resumebtn1 = document.getElementById("resume-button-1").addEventListener("click",()=>{
+  window.location.assign("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing")
+})
