f3e483e3a15b474e43f8ec4906b20733f9972774
deepak chourasiya <deepak1812002@gmail.com>
2023-03-19 01:26:25 +0530

    added in masai

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function closemenu(){
   sidemenu.style.right = "-200px";
 }
 
-function open_in_newtab(){
-  window.open("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing");
-}
+// function open_in_newtab(){
+//   window.open("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing");
+// }
 
-let resumebtn1 = document.getElementById("resume-button-1").addEventListener("click",()=>{
-  window.location.assign("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing")
-})
+// let resumebtn1 = document.getElementById("resume-button-1").addEventListener("click",()=>{
+//   window.location.assign("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing")
+// })
