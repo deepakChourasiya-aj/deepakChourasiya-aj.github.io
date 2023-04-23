5a00bea9502ed86f2834dc03278210932d51454f
deepak chourasiya <deepak1812002@gmail.com>
2023-04-23 19:02:19 +0530

    link google form to contact

--- index.js
+++ index.js
@@ -14,3 +14,6 @@ function closemenu(){
 // let resumebtn1 = document.getElementById("resume-button-1").addEventListener("click",()=>{
 //   window.location.assign("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing")
 // })
+function homePage(){
+  window.location.href='#home'
+}
